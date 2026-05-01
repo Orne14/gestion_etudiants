8a451f110890e927b78fd444b0dfdc9967e6c6a3
Fandy Ornela <ornelafandi@gmail.com>
2026-05-02 01:24:17 +0200

    Création du formulaire avec récupération dynamiques des filières

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 try {
     $db = new PDO(
-        "mysql:host=localhost;dbname=gestion_etudiants",
+        "mysql:host=localhost;dbname=gestion_etudiants;charset=utf8mb4",
         "root",
         ""
     );
--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
-require_once "config/db.php";
+require "config/db.php";
+
+//Récupération des filières
+$filieres = $db->query("SELECT * FROM filieres")->fetchAll();
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des étudiants</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <form action="traitement.php" method="POST">
+    <h2>Ajouter un étudiant</h2>
+
+    <input type="text" name="nom" placeholder="Nom" required>
+    <input type="text" name="prenom" placeholder="Prénom" required>
+
+    <select name="filiere_id" required>
+        <option value="">Choisir une filière</option>
+
+        <?php foreach($filieres as $filiere): ?>
+            <option value="<?= $filiere['id'] ?>">
+                <?= $filiere['nom'] ?>
+            </option>
+        <?php endforeach; ?>
+
+    </select>
+
+    <button type="submit">Ajouter</button>
+</form>
+</body>
+</html>
